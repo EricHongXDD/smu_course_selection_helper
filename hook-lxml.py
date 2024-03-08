d2f8ad5fbfe9e7208dce0ee8542cb9c48ac27ddc
Hong <isaachong127@gmail.com>
2024-03-08 22:40:29 +0800

    Delete hook-lxml.py

--- hook-lxml.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
-hiddenimports = (
-    collect_submodules('lxml') +
-    collect_data_files('lxml'))
-datas = collect_data_files('lxml')
